ca01461b19831f70e1a9bea84e82c29c136037b2
gbili <accounts.guillermo@gmail.com>
2020-06-20 10:27:48 +0200

    fix: test whether this specific module is installed

--- src/loaders/MTB_ENV.ts
+++ src/loaders/MTB_ENV.ts
@@ -4,10 +4,13 @@ import path from 'path';
 import promiseFs from '../utils/promiseFs';
 
 const loadDictElement: LoadDictElement<Promise<string>> = {
-  factory: async function determineIfInstalledViaNpmI({ userProjectRootDir }: { userProjectRootDir: string }) {
-    const nodeModulesDirAssumingNpmInstallEnv = path.resolve(`${__dirname}/../../../`);
-    const wasInstalledViaNpmi = await promiseFs.existsDir(nodeModulesDirAssumingNpmInstallEnv)
+  factory: async function determineIfInstalledViaNpmI({ MTB_PACKAGE_NAME }: { MTB_PACKAGE_NAME: string; }) {
+    const nodeModulesDirAssumingNpmInstallEnv = path.resolve(`${__dirname}/../../../node_modules/${MTB_PACKAGE_NAME}`);
+    const wasInstalledViaNpmi = await promiseFs.existsDir(nodeModulesDirAssumingNpmInstallEnv);
     return wasInstalledViaNpmi ? 'module' : 'clone';
+  },
+  locateDeps: {
+    MTB_PACKAGE_NAME: 'MTB_PACKAGE_NAME',
   },
 };
 export default loadDictElement;
