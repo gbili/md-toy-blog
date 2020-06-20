98355f516061f137c5907c82d73bfedfa7284785
gbili <accounts.guillermo@gmail.com>
2020-06-20 16:25:58 +0200

    feat: log ROOT_DIR

--- src/loaders/MTB_ROOT_DIR.ts
+++ src/loaders/MTB_ROOT_DIR.ts
@@ -3,7 +3,12 @@ import { LoadDictElement } from 'di-why/build/src/DiContainer';
 import path from 'path';
 
 const loadDictElement: LoadDictElement<string> = {
-  instance : path.resolve(`${__dirname}/../../..`),
+  instance : (() => {
+    const rootDir = path.resolve(`${__dirname}/../../..`);
+    console.log('MTB_ENV:ROOT_DIR__dirname:', __dirname);
+    console.log('MTB_ENV:ROOT_DIR:', rootDir);
+    return rootDir;
+  })(),
 };
 
 export default loadDictElement;
