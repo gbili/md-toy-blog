07ce9b3fc121da3f931c42c03ababae70ca36a1b
gbili <accounts.guillermo@gmail.com>
2020-06-20 15:33:35 +0200

    fix: usable MTB_ENV decider

--- src/loaders/MTB_ENV.ts
+++ src/loaders/MTB_ENV.ts
@@ -1,16 +1,11 @@
 import 'dotenv/config';
 import { LoadDictElement } from 'di-why/build/src/DiContainer';
-import path from 'path';
 import promiseFs from '../utils/promiseFs';
+import moduleOrClonedRepo from '../utils/moduleOrClonedRepo';
 
 const loadDictElement: LoadDictElement<Promise<string>> = {
-  factory: async function determineIfInstalledViaNpmI({ MTB_PACKAGE_NAME }: { MTB_PACKAGE_NAME: string; }) {
-    const nodeModulesDirAssumingNpmInstallEnv = path.resolve(`${__dirname}/../../../../node_modules/${MTB_PACKAGE_NAME}`);
-    const wasInstalledViaNpmi = await promiseFs.existsDir(nodeModulesDirAssumingNpmInstallEnv);
-    return wasInstalledViaNpmi ? 'module' : 'clone';
-  },
-  locateDeps: {
-    MTB_PACKAGE_NAME: 'MTB_PACKAGE_NAME',
+  factory: async function determineIfInstalledViaNpmI() {
+    return await moduleOrClonedRepo.isWithinNodeModuleOrClonedRepo(__dirname, promiseFs.existsDir);
   },
 };
 export default loadDictElement;
--- src/utils/getPostPreviewShortener.ts
+++ src/utils/getPostPreviewShortener.ts
@@ -1,14 +1,20 @@
-  export default function getPostPreviewShortener({ previewLength }: { previewLength: number }): (data: FmDataInterface) => FmDataInterface {
-    return function(data: FmDataInterface) {
-      const preview = typeof data.attributes.description !== 'undefined'
-        ? data.attributes.description
-        : data.body.replace(/(```[a-z]*\n[\s\S]*?\n```)/sg, "");
-      const getLines = (text: string) => text.match(/([^\n]+)\n/g);
-      const getFirstLineOrDefault = (preg: RegExpMatchArray | null) => preg !== null ? preg[0] : "You'll need to click to know more";
-      const truncatePreview = (pre: string, maxLen: number) => pre.substring(0, maxLen);
-      return {
-        ...data,
-        body: truncatePreview(getFirstLineOrDefault(getLines(preview)), previewLength)
-      };
+ export default function getPostPreviewShortener({ previewLength }: { previewLength: number }): (data: FmDataInterface) => FmDataInterface {
+  return function(data: FmDataInterface) {
+    const preview = typeof data.attributes.description !== 'undefined'
+      ? data.attributes.description
+      : removeMdCodeBlocks(data.body);
+    const truncatePreview = (pre: string, maxLen: number) => pre.substring(0, maxLen);
+    return {
+      ...data,
+      body: truncatePreview(getFirstLineOrDefault(textToLinesArray(preview)), previewLength)
     };
-  }
+  };
+}
+
+export const removeMdCodeBlocks = function (mdText: string) {
+  return mdText.replace(/(```[a-z]*\n[\s\S]*?\n```)/sg, "");
+}
+
+export const textToLinesArray = (text: string) => text.match(/([^\n]+)\n/g);
+
+export const getFirstLineOrDefault = (preg: RegExpMatchArray | null, defaultText: string = "You'll need to click to know more") => preg !== null ? preg[0] : defaultText;
