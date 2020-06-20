a8bc8ddd081622146e45a60fe069749ab9af28c5
gbili <accounts.guillermo@gmail.com>
2020-06-20 15:52:07 +0200

    fix: acknowledge for new ./src in tsc appearance

--- src/loaders/MTB_ROOT_DIR.ts
+++ src/loaders/MTB_ROOT_DIR.ts
@@ -3,7 +3,7 @@ import { LoadDictElement } from 'di-why/build/src/DiContainer';
 import path from 'path';
 
 const loadDictElement: LoadDictElement<string> = {
-  instance : path.resolve(`${__dirname}/../..`),
+  instance : path.resolve(`${__dirname}/../../..`),
 };
 
 export default loadDictElement;
--- src/loaders/MTB_USER_PROJECT_ROOT_DIR.ts
+++ src/loaders/MTB_USER_PROJECT_ROOT_DIR.ts
@@ -5,8 +5,8 @@ import path from 'path';
 const loadDictElement: LoadDictElement<string> = {
   factory: function ({ MTB_ENV }) {
     const userProjectRootDir = MTB_ENV == 'clone'
-      ? `${__dirname}/../..` // when git cloned, the root is two dirs appart from ./loaders
-      : `${__dirname}/../../../..`; // when installed via npm i, it will be 4 dirs appart (within node_modules)
+      ? `${__dirname}/../../..` // when git cloned, the root is two dirs appart from ./loaders
+      : `${__dirname}/../../../../..`; // when installed via npm i, it will be 5 dirs appart (within node_modules)
     return path.resolve(userProjectRootDir);
   },
   locateDeps: {
