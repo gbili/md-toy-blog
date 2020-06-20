1035896ad71cbec7cf548e3995bf15359380847b
gbili <accounts.guillermo@gmail.com>
2020-06-20 12:37:12 +0200

    fix: point to node modules

--- src/loaders/MTB_ENV.ts
+++ src/loaders/MTB_ENV.ts
@@ -5,7 +5,7 @@ import promiseFs from '../utils/promiseFs';
 
 const loadDictElement: LoadDictElement<Promise<string>> = {
   factory: async function determineIfInstalledViaNpmI({ MTB_PACKAGE_NAME }: { MTB_PACKAGE_NAME: string; }) {
-    const nodeModulesDirAssumingNpmInstallEnv = path.resolve(`${__dirname}/../../../node_modules/${MTB_PACKAGE_NAME}`);
+    const nodeModulesDirAssumingNpmInstallEnv = path.resolve(`${__dirname}/../../../../node_modules/${MTB_PACKAGE_NAME}`);
     const wasInstalledViaNpmi = await promiseFs.existsDir(nodeModulesDirAssumingNpmInstallEnv);
     return wasInstalledViaNpmi ? 'module' : 'clone';
   },
