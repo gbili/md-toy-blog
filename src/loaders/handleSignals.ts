52a7e8acc677b51c7ad52035d595300f22632e5e
gbili <accounts.guillermo@gmail.com>
2020-07-26 12:02:52 +0200

    refactor: handle sigint with server-handle-signals module

--- src/loaders/handleSignals.ts
+++ src/loaders/handleSignals.ts
@@ -1,32 +1,11 @@
 import http from 'http';
 import { LoadDictElement } from 'di-why/build/src/DiContainer';
+import handleSignals from 'server-handle-signals';
 
 export type Server = http.Server;
 
 const loadDictElement: LoadDictElement<(server: Server) => Server> = {
-  instance: (server: Server): Server => {
-    const shutdown = () => {
-      server.close(function onServerClosed(err) {
-        if (err) {
-          console.log(err);
-          process.exitCode = 1;
-        }
-        process.exit();
-      });
-    };
-
-    process.on('SIGTERM', () => {
-      console.log(`Got SIGTERM (docker container stop). Graceful shutdown.`, new Date().toISOString())
-      shutdown();
-    });
-
-    process.on('SIGINT', () => {
-      console.log(`Got SIGINT (aka ctrl-c in docker). Graceful shutdown.`, new Date().toISOString())
-      shutdown();
-    });
-
-    return server;
-  },
+  instance: handleSignals,
 };
 
 export default loadDictElement;
