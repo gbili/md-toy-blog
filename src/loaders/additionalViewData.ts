d948e56a5e3cdee9dcb249da2fb0a2f0ce9c6aff
gbili <accounts.guillermo@gmail.com>
2020-09-12 20:45:53 +0200

    feat: prefix posts links with blog

--- src/loaders/additionalViewData.ts
+++ src/loaders/additionalViewData.ts
@@ -2,7 +2,15 @@ import { LoadDictElement } from 'di-why/build/src/DiContainer';
 import additionalViewData from '../config/additionalViewData';
 
 const loadDictElement: LoadDictElement<{ [k: string]: string; }> = {
-  instance: additionalViewData,
+  factory: function ({ blogUrlPathPrefix }: { blogUrlPathPrefix: string; }) {
+    return {
+      ...additionalViewData,
+      blogUrlPathPrefix,
+    }
+  },
+  locateDeps: {
+    blogUrlPathPrefix: 'MTB_BLOG_URL_PATH_PREFIX',
+  },
 };
 
 export default loadDictElement;
